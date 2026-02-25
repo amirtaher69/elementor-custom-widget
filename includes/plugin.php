bcad84aadc83d2328ee69d512072ca6dfab299bc
amirtaher69 <amirtaheri3569@gmail.com>
2026-02-25 23:07:28 +0330

    complete plugin class

--- includes/plugin.php
+++ includes/plugin.php
@@ -4,5 +4,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class ECW_Plugin {
-    
+    public function __construct() {
+        $this->register_hooks();
+    }
+
+    private function register_hooks() {
+        add_action( 'plugins_loaded', array( $this, 'init' ) );
+    }
+
+    public function init() {
+        if ( ! $this->is_elementor_active() ) {
+            $this->register_admin_notice();
+            return;
+        }
+        $this->init_widgets();
+    }
+
+    private function is_elementor_active() {
+        return did_action( 'elementor/loaded' );
+    }
+
+    private function register_admin_notice() {
+        add_action( 'admin_notices', array( $this, 'admin_notice_missing_elementor' ) );
+    }
+
+    public function admin_notice_missing_elementor() {
+        ?>
+        <div class="notice notice-error">
+            <p>
+                این افزونه برای فعالیت به Elementor نیاز دارد.
+            </p>
+        </div>
+        <?php
+    }
+
+    private function init_widgets() {
+        // add your custom widgets here
+    }
 }
