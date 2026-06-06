7e8f66dfec6a168f52234f0c03c2b06edd2380ad
amirtaher69 <amirtaheri3569@gmail.com>
2026-06-06 20:19:03 +0330

    add basic controls

--- includes/widgets/class-ecw-simple-widget.php
+++ includes/widgets/class-ecw-simple-widget.php
@@ -24,10 +24,45 @@
         return ['general'];
     }
 
+    protected function register_controls() {
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => 'تنظیمات محتوا',
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        // add controls here
+        $this->add_control(
+            'title',
+            [
+                'label' => 'عنوان',
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => 'عنوان ویجت را وارد کنید',
+                'default' => 'من عنوان ویجت هستم',
+            ]
+        );
+
+        $this->add_control(
+            'title_color',
+            [
+                'label' => 'رنگ عنوان',
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'default' =>'#222'
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
     protected function render() {
+        $settings = $this->get_settings_for_display();
         ?>
         <div class="ecw-simple-widget">
-            <h2>سلام . این اولین ویجت المنتور ماست</h2>
+            <h2 style="color: <?php echo esc_attr($settings['title_color']); ?>;">
+                <?php echo esc_html($settings['title']); ?>
+            </h2>
         </div>
         <?php
     }
